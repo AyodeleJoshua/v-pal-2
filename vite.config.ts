829855a31f47885393dcddafb2eb3817a617a199
jayodele64 <jayodele64@gmail.com>
2024-11-12 17:41:41 +0100

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -8,4 +9,7 @@ export default defineConfig({
     port: 3000,
   },
   base: '/v-pal-2/',
+  optimizeDeps: {
+    exclude: ['js-big-decimal'],
+  },
 });
